c20bf1660a21817411d1eb60f23bed5c99b64b91
taitt-e <taittestes20@outlook.com>
2023-03-30 15:17:47 -0500

    Signed Comments.

--- login.js
+++ login.js
@@ -1,22 +1,25 @@
-//Needed Global const for the page to function properly. - Taitt
+//Needed Globals for the page to function properly. - Taitt
 const loginButton = document.getElementById("login-submit");
 const login = document.getElementById("login"); 
 var loginErrorMsg = "Invalid Credentials. Try Again.";
 
-//Login Button Function, for now checks if their user and pass is admin then allows login.
-//
+//Login Button Function, for now checks if their user and pass is admin then allows login. - Taitt
 loginButton.onclick = function(){
+    //Pick up work here again some time later, Data needs hidden & redirection to gameSelect needs fixed. - Taitt
     let user = login.elements.item(0).value;
     let pass = login.elements.item(1).value;
-    //document.getElementById("login").elements.item(0).value;
 
-    //Added for testing purposes.
+    //document.getElementById("login").elements.item(0).value; Template to follow - Taitt
+
+    //Added for testing purposes. - Taitt
     console.log("Username is: " + user);
     console.log("Password is: " + pass);
 
     if (user == "admin" && pass == "admin") {
         alert("You have successfully logged in.");
-        window.location.assign("gameSelect.html");
+        
+        //Redirect to gameSelect.html - Taitt
+        //window.location.assign("gameSelect.html");
     } else {
         alert(loginErrorMsg);
     }
