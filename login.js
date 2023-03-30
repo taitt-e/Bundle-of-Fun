61f861ce6877538932f135df582784788c441ec7
taitt-e <taittestes20@outlook.com>
2023-03-30 14:50:35 -0500

    Fixed Login.

--- login.js
+++ login.js
@@ -6,12 +6,15 @@ var loginErrorMsg = "Invalid Credentials. Try Again.";
 //Login Button Function, for now checks if their user and pass is admin then allows login.
 //
 loginButton.onclick = function(){
-    //Added for testing purposes.
-    console.log("Username is: " + username);
-    console.log("Password is: " + password);
+    let user = login.elements.item(0).value;
+    let pass = login.elements.item(1).value;
+    //document.getElementById("login").elements.item(0).value;
 
-    if (username == "admin" //*/&& password == "admin"*/
-    ) {
+    //Added for testing purposes.
+    console.log("Username is: " + user);
+    console.log("Password is: " + pass);
+
+    if (user == "admin" && pass == "admin") {
         alert("You have successfully logged in.");
         //location.reload();
     } else {
