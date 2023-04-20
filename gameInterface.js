ff0f5e357990e2d241be10475615d591eb53f929
nsantil <114325261+nsantil@users.noreply.github.com>
2023-04-20 01:27:11 -0500

    Update gameInterface.js
    added breakout game

--- gameInterface.js
+++ gameInterface.js
@@ -20,11 +20,41 @@ class GameInterfaceTemplate{
 }
 */
 //"Base" Classes applied in "Assistant" CLasses
+
+const paddle = {
+    // place the paddle horizontally in the middle of the screen
+    x: canvas.width / 2 - brickWidth / 2,
+    y: 440,
+    width: brickWidth,
+    height: brickHeight,
+  
+    // paddle x velocity
+    dx: 0
+};
+
+const ball = {
+    x: 130,
+    y: 260,
+    width: 5,
+    height: 5,
+  
+    // how fast the ball should go in either the x or y direction
+    speed: 2,
+  
+    // ball velocity
+    dx: 0,
+    dy: 0
+  };
+
+  
+
 class PlayerObjTemplate{
     //Needed constructor to function. - Taitt
-    constructor(x,y,dx,dy, graphicConst){
+    constructor(x,y,width, height, dx,dy, graphicConst){
         this.x = x;
         this.y = y;
+        this.width = width;
+        this.height = height;
         this.dx = dx;
         this.dy = dy;
         this.graphicConst = graphicConst;
@@ -123,41 +153,188 @@ class UI1 extends UserInterfaceTemplate{
     }
     //This will be for the user's Keyboard, allowing multiple key inputs - Taitt
     userControl(){
-        document.addEventListener('keydown', function(event) {
-            if(event.keyCode == 87) {
-                alert('W was pressed');
+
+        // listen to keyboard events to move the paddle
+        document.addEventListener('keydown', function(e) {
+            // A key
+            if (e.which === 65) {
+              paddle.dx = -3;
             }
-            else if(event.keyCode == 83) {
-                alert('S was pressed');
+            // D key
+            else if (e.which === 68) {
+              paddle.dx = 3;
             }
-            else if(event.keyCode == 65){
-                alert('A was pressed');
-            }
-            else if(event.keyCode == 68){
-                alert('D was pressed');
+          
+            // space key
+            // if they ball is not moving, we can launch the ball using the space key. ball
+            // will move towards the bottom right to start
+            if (ball.dx === 0 && ball.dy === 0 && e.which === 32) {
+              ball.dx = ball.speed;
+              ball.dy = ball.speed;
             }
         });
+          
+        // listen to keyboard events to stop the paddle if key is released
+        document.addEventListener('keyup', function(e) {
+            if (e.which === 65 || e.which === 68) {
+              paddle.dx = 0;
+            }
+        });
+        
     }
 }
 
 class Graphics1 extends GraphicsTemplate{
     graphicsGeneration(){
         //Insert Code Here
+        const canvas = document.getElementById('game');
+        const context = canvas.getContext('2d');
+        context.clearRect(0,0,canvas.width,canvas.height);
+
+        const level1 = [
+            [],
+            [],
+            [],
+            [],
+            [],
+            [],
+            ['R','R','R','R','R','R','R','R','R','R','R','R','R','R'],
+            ['R','R','R','R','R','R','R','R','R','R','R','R','R','R'],
+            ['O','O','O','O','O','O','O','O','O','O','O','O','O','O'],
+            ['O','O','O','O','O','O','O','O','O','O','O','O','O','O'],
+            ['G','G','G','G','G','G','G','G','G','G','G','G','G','G'],
+            ['G','G','G','G','G','G','G','G','G','G','G','G','G','G'],
+            ['Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y'],
+            ['Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y']
+        ];
+
+        const colorMap = {
+            'R': 'red',
+            'O': 'orange',
+            'G': 'green',
+            'Y': 'yellow'
+        };
+
+        const brickGap = 2;
+        const brickWidth = 30;
+        const brickHeight = 12;
+
+        const wallSize = 3;
+        const bricks = [];
+
+        // draw walls
+        context.fillStyle = 'lightgrey';
+        context.fillRect(0, 0, canvas.width, wallSize);
+        context.fillRect(0, 0, wallSize, canvas.height);
+        context.fillRect(canvas.width - wallSize, 0, wallSize, canvas.height);
+
+        // draw ball if it's moving
+        if (ball.dx || ball.dy) {
+            context.fillRect(ball.x, ball.y, ball.width, ball.height);
+        }
+
+        // draw bricks
+        bricks.forEach(function(brick) {
+            context.fillStyle = brick.color;
+            context.fillRect(brick.x, brick.y, brick.width, brick.height);
+        });
+
+        // draw paddle
+        context.fillStyle = 'cyan';
+        context.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+
     }
     //Insert Code Here
 }
 
 class GameLogic1 extends GameLogicTemplate{
     //Insert Code Here
+    collides(obj1, obj2) {
+        return obj1.x < obj2.x + obj2.width && obj1.x + obj1.width > obj2.x && obj1.y < obj2.y + obj2.height && obj1.y + obj1.height > obj2.y;
+    }
+    
 }
 
 class Game1 extends GameTemplate{
     //This is an example for testing - Taitt Estes
     //Needed let vars here
+
     gameLoop(){
         //Insert needed "let" variables here for game functionality, then run the loop. - Taitt
         while(true){
             //Insert Code Here.
+            
+            paddle.x += paddle.dx;
+    
+            // prevent paddle from going through walls
+            if (paddle.x < wallSize) {
+            paddle.x = wallSize
+            }
+            else if (paddle.x + brickWidth > canvas.width - wallSize) {
+            paddle.x = canvas.width - wallSize - brickWidth;
+            }
+        
+            // move ball by it's velocity
+            ball.x += ball.dx;
+            ball.y += ball.dy;
+        
+            // prevent ball from going through walls by changing its velocity
+            // left & right walls
+            if (ball.x < wallSize) {
+            ball.x = wallSize;
+            ball.dx *= -1;
+            }
+            else if (ball.x + ball.width > canvas.width - wallSize) {
+            ball.x = canvas.width - wallSize - ball.width;
+            ball.dx *= -1;
+            }
+            // top wall
+            if (ball.y < wallSize) {
+            ball.y = wallSize;
+            ball.dy *= -1;
+            }
+        
+            // reset ball if it goes below the screen
+            if (ball.y > canvas.height) {
+            ball.x = 130;
+            ball.y = 260;
+            ball.dx = 0;
+            ball.dy = 0;
+            }
+    
+            if(collides(ball, paddle)){
+                ball.dy *= -1;
+            
+                // move ball above the paddle otherwise the collision will happen again
+                // in the next frame
+                ball.y = paddle.y - ball.height;
+            }
+
+            // check to see if ball collides with a brick. if it does, remove the brick
+            // and change the ball velocity based on the side the brick was hit on
+            for(let i = 0; i < bricks.length; i++){
+                const brick = bricks[i];
+            
+                if (collides(ball, brick)) {
+                // remove brick from the bricks array
+                bricks.splice(i, 1);
+            
+                // ball is above or below the brick, change y velocity
+                // account for the balls speed since it will be inside the brick when it
+                // collides
+                if (ball.y + ball.height - ball.speed <= brick.y ||
+                    ball.y >= brick.y + brick.height - ball.speed) {
+                    ball.dy *= -1;
+                }
+                // ball is on either side of the brick, change x velocity
+                else {
+                    ball.dx *= -1;
+                }
+            
+                break;
+                }
+            }
+            
             return;
         }
     }
@@ -165,9 +342,9 @@ class Game1 extends GameTemplate{
 
 
 //Just another example.
-const thisPlayerObj = new PlayerObj1(null,null,null,null);
-const thisObj = new Obj1(null,null,null,null,null);
-const thisUI = new UI1(null, thisPlayerObj);
-const thisGraphics = new Graphics1(null, thisPlayerObj, thisObj);
-const thisGameLogic = new GameLogic1(thisPlayerObj, thisObj);
-const thisGame = new Game1("NewGame","InsertCSS.css", thisUI, thisGraphics, thisGameLogic, thisUserControl);
+//const thisPlayer = new PlayerObj1((canvas.width / 2) - (brickWidth / 2), 440,null,null);
+// const thisObj = new Obj1(null,null,null,null,null);
+// const thisUI = new UI1(null, thisPlayerObj);
+// const thisGraphics = new Graphics1(null, thisPlayerObj, thisObj);
+// const thisGameLogic = new GameLogic1(thisPlayerObj, thisObj);
+// const thisGame = new Game1("NewGame","InsertCSS.css", thisUI, thisGraphics, thisGameLogic, thisUserControl);
